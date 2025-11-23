3fcd1f45df5ce33225b04b29c2b97cfe6e3a4bfd
Bombele Liyama Camille <94522767+Bombele@users.noreply.github.com>
2025-11-23 19:18:08 -0400

    Update session.py

--- apps/api/db/session.py
+++ apps/api/db/session.py
@@ -1,1 +1,14 @@
-.
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+# URL de la base de datos
+# Para empezar usamos SQLite local, luego puedes cambiar a PostgreSQL o MySQL
+DATABASE_URL = "sqlite:///./itcaa.db"
+
+# Crear el motor de conexión
+engine = create_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False}  # Necesario solo para SQLite
+)
+
+# Crear la fábrica de sesiones
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
