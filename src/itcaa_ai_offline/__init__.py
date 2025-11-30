6ade6f31d725dcbb0abe31ecfe2e26cf6c792b0b
Bombele Liyama Camille <94522767+Bombele@users.noreply.github.com>
2025-11-30 11:27:09 -0400

    Update __init__.py

--- src/itcaa_ai_offline/__init__.py
+++ src/itcaa_ai_offline/__init__.py
@@ -1,1 +1,13 @@
+# src/itcaa_ai_offline/__init__.py
+
+# Import explicite des sous-modules
+from . import config
+from .data.corpus import index_builder
+# Vérifie que predictor existe avant de l'importer
+try:
+    from . import predictor
+except ImportError:
+    predictor = None
+
+# Définition de l'API publique
 __all__ = ["config", "index_builder", "predictor"]
