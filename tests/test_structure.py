81c8dc71e3fd003c3724e9549fe25a1c3692bb97
Bombele Liyama Camille <94522767+Bombele@users.noreply.github.com>
2025-12-02 10:39:45 -0400

    Actualizar test_structure.py

--- tests/test_structure.py
+++ tests/test_structure.py
@@ -11,3 +11,11 @@ def test_import_module():
 
     # Vérifie que la docstring existe
     assert hasattr(itcaa_ai_offline, "__doc__"), "Le module doit avoir une docstring"
+
+def test_imports_structure():
+    from src.itcaa_ai_offline.schemas import PredictionInput, PredictionOutput
+    from src.itcaa_ai_offline.utils import normalize_features, log_prediction
+    assert callable(normalize_features)
+    assert callable(log_prediction)
+    assert PredictionInput is not None
+    assert PredictionOutput is not None
