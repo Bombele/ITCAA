2ed2cb9bdc95a2b253d4e310854c2edcf3685ba8
Bombele Liyama Camille <94522767+Bombele@users.noreply.github.com>
2025-12-01 21:26:59 -0400

    Actualizar test_faiss_index.py

--- tests/test_faiss_index.py
+++ tests/test_faiss_index.py
@@ -5,7 +5,7 @@ def test_faiss_index_valid():
     # Vérifie que le fichier index existe
     assert PATHS.faiss_index.exists(), "Fichier FAISS index manquant"
 
-    # Charge l'index
+    # Charge l'index FAISS
     index = faiss.read_index(str(PATHS.faiss_index))
 
     # Vérifie qu'il contient au moins un vecteur
